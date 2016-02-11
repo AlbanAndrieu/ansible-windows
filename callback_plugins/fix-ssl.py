0d9e7dedee79517e059d05ddd0681c7f2db2d938
Alban Andrieu <alban.andrieu@misys.com>
2016-02-11 04:22:37 -0600

    Add callback

---
+++ callback_plugins/fix-ssl.py
@@ -0,0 +1,8 @@
+import ssl
+if hasattr(ssl, '_create_default_https_context') and hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+class CallbackModule(object):
+    pass
+
+# needed to bypass self-signed certificates for winrm
